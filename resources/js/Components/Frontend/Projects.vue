2a4e7c764e081468ae8dfe9e259b0740256e5db7
xSpaceNekox <rainbowchicken73@gmail.com>
2023-10-15 20:52:19 +0800

    Can now Filter Projects

--- resources/js/Components/Frontend/Projects.vue
+++ resources/js/Components/Frontend/Projects.vue
@@ -1,9 +1,26 @@
 <script setup>
 import Project from './Project.vue';
-defineProps({
+import { ref } from 'vue';
+
+const props = defineProps({
     skills: Object,
     projects: Object,
 });
+
+const filteredProjects = ref(props.projects.data);
+const selectedSkill = ref("all");
+
+const filterProjects = (id) => {
+    if (id === "all") {
+        filteredProjects.value = props.projects.data
+        selectedSkill.value = id;
+    } else {
+        filteredProjects.value = props.projects.data.filter(project => {
+            return project.skill.id === id;
+        });
+        selectedSkill.value = id;
+    }
+};
 </script>
 
 <template>
@@ -11,15 +28,19 @@ defineProps({
         <nav class="mb-12 border-b-2 border-light-tail-100">
             <ul class="flex flex-col lg:flex-row justify-evenly items-center">
                 <li class="cursor-pointer capitalize m-4">
-                    <button class="flex text-center px-4 py-2 hover:text-light-tail-500">All</button>
+                    <button @click="filterProjects('all')" class="flex text-center px-4 py-2 hover:text-light-tail-500"
+                        :class="[selectedSkill === 'all' ? 'text-light-tail-500' : '',]">All</button>
                 </li>
                 <li class="cursor-pointer capitalize m-4" v-for="projectSkill in skills.data" :key="projectSkill.id">
-                    <button class="flex text-center px-4 py-2 hover:text-light-tail-500"> {{ projectSkill.name }}</button>
+                    <button @click="filterProjects(projectSkill.id)"
+                        class="flex text-center px-4 py-2 hover:text-light-tail-500"
+                        :class="[selectedSkill === projectSkill.id ? 'text-light-tail-500' : '',]">
+                        {{ projectSkill.name }}</button>
                 </li>
             </ul>
         </nav>
         <section class="grid gap-y-12 lg:grid-cols-3 lg:gap-8">
-            <Project v-for="project in projects.data" key="project.id" :project="project" />
+            <Project v-for="project in filteredProjects" key="project.id" :project="project" />
         </section>
     </div>
 </template>
